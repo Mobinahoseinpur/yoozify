92e46b42f311d2ca176159a9e5d55f942b1e3a07
mobina.hoseinpur1377 <mobina.hoseinpur1377@gmail.com>
2022-04-03 11:51:51 +0430

    start to content

---
+++ src/component/content/content.jsx
@@ -0,0 +1,17 @@
+// for content
+
+import React,{Fragment} from 'react'
+import styles from '../content/Content.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const Content = () => {
+    return ( 
+        <Fragment>
+            <div className={`${styles.content_} content`}>
+                back
+            </div>
+        </Fragment>
+     );
+}
+ 
+export default Content;
