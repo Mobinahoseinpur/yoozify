3c82a511df26e6f68e85d1a334222a976f5464a4
mobina.hoseinpur1377 <mobina.hoseinpur1377@gmail.com>
2022-04-19 11:22:48 +0430

    start to create google map

--- src/component/about us page/AboutUsPage.jsx
+++ src/component/about us page/AboutUsPage.jsx
@@ -1,15 +1,24 @@
 import React,{Fragment} from 'react'
 import Content from '../content/Content'
 import styles from './AboutUsPage.module.css'
+
 const AboutUsPage = () => {
+
+
     return ( 
         <Fragment>
             <Content>
-                <div className="">
-                    <div className={``}>about Us</div>
-                    <div className={``}>about Us</div>
+                <div className={`${styles.mainbox}`}>
+                    <div className={`${styles.box1}`}>
+                        <label className={`${styles.partitiontitle}`}> ارتباط با ما</label>
+                        <input type="text" className={`${styles.title}`} placeholder="عنوان" />
+                        <input type="text" className={`${styles.massegebox}`} placeholder="متن..."/>
+                        <input type="button" className={`${styles.sendbtn}`} value="ارسال" />
+ 
+                    </div>
+                    <div className={`${styles.box2}`}></div>
                 </div>
-                <div className={``}>about Us</div>
+                <div className={`${styles.mapbox}`}>about Us</div>
             </Content>
         </Fragment>
      );
