28e71173d3ed690feed3b552da5ae07d37ac1c50
mobina.hoseinpur1377 <mobina.hoseinpur1377@gmail.com>
2022-04-21 16:29:41 +0430

    install goolge map

--- src/component/about us page/AboutUsPage.jsx
+++ src/component/about us page/AboutUsPage.jsx
@@ -1,10 +1,21 @@
 import React,{Fragment} from 'react'
 import Content from '../content/Content'
 import styles from './AboutUsPage.module.css'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 const AboutUsPage = () => {
 
+    const center = {
+        lat: -3.745,
+        lng: -38.523
+      };
 
+      const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "YOUR_API_KEY"
+      })
+
+      
     return ( 
         <Fragment>
             <Content>
@@ -13,12 +24,15 @@ const AboutUsPage = () => {
                         <label className={`${styles.partitiontitle}`}> ارتباط با ما</label>
                         <input type="text" className={`${styles.title}`} placeholder="عنوان" />
                         <input type="text" className={`${styles.massegebox}`} placeholder="متن..."/>
+
                         <input type="button" className={`${styles.sendbtn}`} value="ارسال" />
  
                     </div>
                     <div className={`${styles.box2}`}></div>
                 </div>
-                <div className={`${styles.mapbox}`}>about Us</div>
+                <div className={`${styles.mapbox}`}>
+
+                </div>
             </Content>
         </Fragment>
      );
